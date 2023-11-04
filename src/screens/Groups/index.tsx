07ebcf250dd42d9968cc52e2af8b29e4ee1919e0
Lucas Dantas <lucasdantasprogramador@gmail.com>
2023-11-04 19:18:29 -0300

    adding babel to project

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import {Groups} from './src/screens/Groups'
-export default function App() {
+
+export function Groups() {
   return (
-    <Groups/>
+    <View style={styles.container}>
+      <Text>groups</Text>
+    </View>
   );
 }
 
