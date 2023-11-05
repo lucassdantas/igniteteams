0c946bd03f52934855af5a4a59a20d566a9facd1
Lucas Dantas <lucasdantasprogramador@gmail.com>
2023-11-04 21:59:12 -0300

    add loading and fonts

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,6 +1,5 @@
 import { StyleSheet, Text, View } from 'react-native';
-
-import { Container } from './styles';
+import { Container} from './styles'
 export function Groups() {
   return (
     <Container>
